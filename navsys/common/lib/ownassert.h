bd83b9ece7ce7fadc810001878b80120a668888f
Grigory Revzin <revzingg@gmail.com>
2015-03-23 14:24:44 +0300

    Исправление в ассерте

--- navsys/common/lib/ownassert.h
+++ navsys/common/lib/ownassert.h
@@ -2,11 +2,11 @@
 #define __OWNASSERT_H__
 
 #ifdef ASSERT_ENABLED
-#define assert(x, p) if (!(x)) __assert_handler((p));
+#define assert(x, s) if (!(x)) __assert_handler((s));
 #else
-#define assert(x, p) ;
+#define assert(x, s) ;
 #endif
 
-void __assert_handler(const char *p);
+void __assert_handler(const char *s);
 
 #endif
