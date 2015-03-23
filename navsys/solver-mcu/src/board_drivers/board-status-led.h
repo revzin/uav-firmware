ea1c9d7b6114ab280988a8b3ef40200075fdb61d
Grigory Revzin <revzingg@gmail.com>
2015-03-23 13:49:40 +0300

    Добавил параметры в декларации

--- navsys/solver-mcu/src/board_drivers/board-status-led.h
+++ navsys/solver-mcu/src/board_drivers/board-status-led.h
@@ -4,12 +4,12 @@
 /* Функции управления светодиодиком на PA7 */
 
 /* подготавливает порт к использованию */
-void BRD_InitStatusLedControl();
+void BRD_InitStatusLedControl(void);
 
 /* зажигает светодиод */
-void BRD_StatusLedOn();
+void BRD_StatusLedOn(void);
 
 /* гасит светодиод */
-void BRD_StatusLedOff();
+void BRD_StatusLedOff(void);
 
 #endif /* __BOARD_STATUS_LED__ */
